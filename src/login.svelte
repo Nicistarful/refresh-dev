69c0e7915aac66669a89408c7c5415e9932c0a50
Nicola <nwurbs1@gmail.com>
2022-03-31 17:43:00 +0200

    Final commit

--- src/login.svelte
+++ src/login.svelte
@@ -1,19 +1,54 @@
 <script>
-	import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+	import {
+		getAuth,
+		signInWithEmailAndPassword,
+		createUserWithEmailAndPassword,
+	} from 'firebase/auth'
+	import {
+		collection,
+		query,
+		where,
+		getDocs,
+		setDoc,
+		doc,
+	} from 'firebase/firestore'
 	import { fade } from 'svelte/transition'
 	import { replace } from 'svelte-spa-router'
-	import { auth } from './stores'
+	import { auth, db } from './stores'
 
 	let loginError = ''
 	let isLoginError = false
 
 	const loginSubmit = async form => {
+		const mail = form.target.elements.email.value
+		const password = form.target.elements.password.value
+		const name = form.target.elements.name.value
 		form.preventDefault()
-		signInWithEmailAndPassword(
-			auth,
-			form.target.elements.email.value,
-			form.target.elements.password.value
-		)
+
+		const usersRef = collection(db, 'users')
+		const q = query(usersRef, where('mail', '==', mail))
+		const snapshot = await getDocs(q)
+		if (snapshot.docs[0] === undefined) {
+			console.log('Email does not exist!')
+			const createdUser = await createUserWithEmailAndPassword(
+				auth,
+				mail,
+				password
+			)
+			const uid = createdUser.user.uid
+			const data = {
+				name: name,
+				mail: mail,
+			}
+			await setDoc(doc(db, 'users', uid), data)
+			window.alert('Account wurde erstellt, bitte nun einloggen')
+			form.target.reset()
+			return
+		} else if (snapshot.docs[0].data().mail === mail) {
+			console.log('Email exists!')
+		}
+
+		signInWithEmailAndPassword(auth, mail, password)
 			.then(userCredential => {
 				// Signed in
 				const user = userCredential.user
@@ -54,6 +89,10 @@
 	<h1 class="text-center text-3xl mb-2">Willkommen bei ReFresh!</h1>
 	<img class="m-auto mb-2" src="../img/ReFresh-Logo128.png" alt="App Logo" />
 	<p class="text-center">Bitte einloggen um fortzufahren</p>
+	<p class="text-center opacity-50 uppercase">
+		Hinweis: Sollten Sie keinen Account haben, bitte im Namesfeld Ihren
+		Namen eingeben
+	</p>
 
 	<div class="absolute top-1/2 left-1/4 right-1/4">
 		<form on:submit={loginSubmit}>
@@ -72,6 +111,12 @@
 				placeholder="Passwort"
 				required
 				minlength="8"
+			/>
+			<input
+				name="name"
+				type="text"
+				class="login-input"
+				placeholder="Vor- und Nachname"
 			/>
 			<input type="submit" class="login-button" value="Log In" />
 		</form>
